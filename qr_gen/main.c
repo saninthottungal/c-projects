deec7785c31ffccbfebbee9d3e4b02b9ec3b3b45
sanin <saninthottungalhere@gmail.com>
2025-11-04 20:40:40 +0530

    qr generation added

--- qr_gen/main.c
+++ qr_gen/main.c
@@ -9,5 +9,26 @@ int main(int argc, char const *argv[])
         return 1;
     }
 
+    QRcode *qrcode = QRcode_encodeString(argv[1], 0, QR_ECLEVEL_L, QR_MODE_8, 0);
+
+    if (qrcode == NULL)
+    {
+        printf("Failed to encode the string into QR code.\n");
+        return 1;
+    }
+
+    // Print ASCII version
+    for (int y = 0; y < qrcode->width; y++)
+    {
+        for (int x = 0; x < qrcode->width; x++)
+        {
+            putchar((qrcode->data[y * qrcode->width + x] & 1) ? '#' : ' ');
+            putchar(' ');
+        }
+        putchar('\n');
+    }
+
+    QRcode_free(qrcode);
+
     return 0;
 }
