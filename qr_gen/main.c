0429458655b30c403f419a9beadf2bde64bfeef2
sanin <saninthottungalhere@gmail.com>
2025-11-04 20:31:55 +0530

    makefile done

--- qr_gen/main.c
+++ qr_gen/main.c
@@ -1,5 +1,13 @@
+#include <stdio.h>
+#include <qrencode.h>
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    if (argc < 2)
+    {
+        printf("Usage: %s <data_to_encode>\n", argv[0]);
+        return 1;
+    }
+
     return 0;
 }
