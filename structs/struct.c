8221dbea49ad6f574bd3018cebe452d166adb2a9
sanin <saninthottungalhere@gmail.com>
2026-09-19 19:01:00 +0530

    passing structure as variable done

--- structs/struct.c
+++ structs/struct.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
 
-void printIdAndName(int id, char *name);
-
 typedef struct {
   int id;
   char name[12];
   int age;
 } Student;
 
+void printIdAndName(int id, char *name);
+void printDetails(Student student);
+
 int main() {
   Student student = {12, "sanin", 22};
 
   printIdAndName(student.id, student.name);
+
+  printDetails(student);
 }
 
 // 1. passing structure memeber as argument
 void printIdAndName(int id, char *name) {
   printf("The ID of the student is %d and name is %s\n", id, name);
+}
+
+// 2. passing structure variable as argument
+void printDetails(Student student) {
+  printf("\nID: %d\nName: %s\nAge: %d\n", student.id, student.name,
+         student.age);
 }
