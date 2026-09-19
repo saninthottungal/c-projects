842d5ea29230008ece53d44dce0ab7a171c0cf4c
sanin <saninthottungalhere@gmail.com>
2026-09-19 20:16:11 +0530

    linked list basics completed

---
+++ dsa/linked_list/linked_list.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  printf("\nWelcome to the Linked list program: ");
+
+  while (1) {
+    int choice;
+
+    printf("\n1. Insert\n2. Delete\n3. Display\n4. Search\n5. Display\n6. "
+           "Exit\n");
+    printf("\nEnter the action you want to perform: ");
+
+    scanf("%d", &choice);
+    printf("\n");
+
+    switch (choice) {
+
+    case 6:
+      printf("Thank you!\n");
+      return 0;
+
+    default:
+      printf("Entered action is invalid, please try again!");
+    }
+
+    printf("\n");
+  }
+
+  return 0;
+}
